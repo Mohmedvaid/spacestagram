15a3fdd95a9b7536fb050b10806158b0a55d13d7
Mohmed Vaid <59095556+Mohmedvaid@users.noreply.github.com>
2022-01-10 17:27:38 -0600

    added search, basic card styles and updated loading element

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,41 +3,32 @@ import Header from './components/ui/Header';
 import Grid from './components/Grid';
 import './App.css';
 import axios from 'axios';
+import Search from './components/ui/Search';
 
 function App() {
   const [collection, setCollection] = useState([]);
   const [isLoading, setisLoading] = useState(true);
   const [photos, setPhotos] = useState([]);
-  let imageAndVidApi = `https://images-api.nasa.gov/search?q=mars&media_type=image&page=1`;
-  let marsPhotoApi = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&sol=1000&page=2`;
+  const [query, setQuery] = useState('galaxy');
+  let imageAndVidApi = `https://images-api.nasa.gov/search?q=${query}&media_type=image&page=1`;
+  const fetchItems = async () => {
+    setisLoading(true);
+    const result = await axios(imageAndVidApi);
+    setCollection(result.data.collection);
+    setPhotos(result.data.collection.items);
+    setisLoading(false);
+  };
+
   useEffect(() => {
-    const fetchItems = async () => {
-      let localData = JSON.parse(localStorage.getItem('nasaData'));
-      if (localData) {
-        console.log('localData', localData);
-        localData = { collection: { items: localData.collection.items.slice(0, 5) } };
-        setCollection(localData.collection);
-        setPhotos(localData.collection.items);
-      } else {
-        let result = await axios.get(imageAndVidApi);
-        setCollection(result.data.collection);
-        setPhotos(result.data.collection.items);
-        localStorage.setItem('nasaData', JSON.stringify(result.data));
-      }
-      setisLoading(false);
-      //   const result = await axios(imageAndVidApi);
-      //   setCollection(result.data.collection);
-      //   setPhotos(result.data.collection.items);
-      //   setisLoading(false);
-    };
     fetchItems();
-  }, []);
+  }, [query]);
   return (
     <div className="container-fluid">
       <div className="row">
         <Header />
       </div>
-        <Grid isLoading={isLoading} photos={photos} />
+      <Search getQuery={(q) => setQuery(q)} />
+      <Grid isLoading={isLoading} photos={photos} />
     </div>
   );
 }
--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,25 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const Card = ({ photo }) => {
-	let data = photo.data[0]
-	let imgSrc = photo.links[0].href
-	// console.log(data)
-	return (
+  let data = photo.data[0];
+  let imgSrc = photo.links[0].href;
+  let dateCreated = new Date(data.date_created).toLocaleDateString('en-US');
+  // console.log(data)
+  return (
     <div className="col-lg-3 col-md-4 col-sm-5 m-t-10">
       <div className="card mb-4">
-        <img style={{ maxWidth: '400px' }} className="card-img-top img-fluid" src={imgSrc} alt={data.description} loading="lazy" />
+        <img className="card-img-top img-fluid" src={imgSrc} alt={data.description} />
         <div className="card-body">
-          <h4 className="card-title">{data.title}</h4>
-          <p className="card-text">
-            <strong>Location:</strong> {data.location}
-          </p>
-          <p className="card-text">
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <h4 className="card-title text-dark">{data.title}</h4>
+          <p className="card-text text-dark">{data.location}</p>
+          <p className="card-text text-dark">
+            <small className="text-muted">{dateCreated}</small>
           </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;

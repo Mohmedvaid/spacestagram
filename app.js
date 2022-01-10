bd8dd55ab836ffc101c1940faf2126168a7508b7
mohmedvaid <hussainvaid7@gmail.com>
2022-01-09 18:26:08 -0600

    removed react place holder, added header component and basic styles

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
+// static routes
+app.use(express.static(path.join(__dirname, 'front-end/build')))
+app.use('/', function (req, res) {
+	return res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`)

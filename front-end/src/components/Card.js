a5bea7b6ae42644dc3668bddd54e81d6f494c792
Mohmed Vaid <59095556+Mohmedvaid@users.noreply.github.com>
2022-01-11 15:25:59 -0600

    added like feature

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BsHeartFill, BsHeart } from 'react-icons/bs';
 
-const Card = ({ photo }) => {
+
+
+const Card = ({ photo, addNewLike, removeLike, isLikedImage }) => {
   let data = photo.data[0];
   let imgSrc = photo.links[0].href;
   let dateCreated = new Date(data.date_created).toLocaleDateString('en-US');
-  // console.log(data)
+  let [isLiked, setIsLiked] = useState(isLikedImage);
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  useEffect(() => {
+    if (isLiked) addNewLike(data.nasa_id);
+    else removeLike(data.nasa_id);
+  }, [isLiked]);
+
   return (
     <div className="m-1">
       <div className="card">
         <img className="card-img-top img-fluid" src={imgSrc} alt={data.description} />
         <div className="card-body">
           <h4 className="card-title text-dark">{data.title}</h4>
-          <p className="card-text text-dark">{data.location}</p>
-          <p className="card-text text-dark">
-            <small className="text-muted">{dateCreated}</small>
-          </p>
+          <div className="d-flex justify-content-between my-1">
+            {/* disbaling text selection on ancher below as clicking like button multiple times was highligting the date text */}
+            <a className="likeBtn disable-text-selection" onClick={() => toggleLike()}>
+              {/* like icon */}
+              <span> {isLiked ? <BsHeartFill size="2em" color="red" /> : <BsHeart size="2em" color="red" />}</span>
+            </a>
+            <p className="card-text text-dark">
+              <small className="text-muted">{dateCreated}</small>
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- front-end/src/components/Grid.js
+++ front-end/src/components/Grid.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import Card from './Card'
-import Spinner from './ui/Spinner'
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import Spinner from './ui/Spinner';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 const Grid = ({ isLoading, photos }) => {
-	return isLoading ? (
-		<Spinner />
-	) : (
-		<section className="row">
-			<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 576: 2, 750: 3, 900: 4 }}>
-				<Masonry>
-					{photos.map((photo) => (
-						<Card key={photo.data[0].nasa_id} photo={photo}></Card>
-					))}
-				</Masonry>
-			</ResponsiveMasonry>
-		</section>
-	)
-}
+	let savedPhotos = JSON.parse(localStorage.getItem('savedPhotos'));
+  const [likedImages, setLikedImages] = useState(savedPhotos ? savedPhotos : []);
 
-export default Grid
+  const addNewLike = (newLike) => {
+      setLikedImages([...likedImages, newLike]);
+  };
+
+// remove the like id from the likedImages array
+  const removeLike=(id)=>{
+	  setLikedImages(likedImages.filter((nasaID) => nasaID !== id));
+  }
+// save liked images to local storage
+  const saveLikes=(likesArr)=>{
+	  localStorage.setItem('savedPhotos', JSON.stringify(likesArr));
+  }
+
+//  updated the likedImages array in local storage every time like is toggled
+  useEffect(() => {
+	  saveLikes(likedImages);
+  } , [likedImages]);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="row">
+      {/* Masonry for better layout and filling empty spaced in columns and rows */}
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 576: 2, 750: 3, 900: 4 }}>
+        <Masonry>
+          {photos.map((photo) => (
+            <Card key={photo.data[0].nasa_id} photo={photo} addNewLike={(id) => addNewLike(id)} removeLike={(id) => removeLike(id)} isLikedImage={likedImages.includes(photo.data[0].nasa_id)}></Card>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </section>
+  );
+};
+
+export default Grid;

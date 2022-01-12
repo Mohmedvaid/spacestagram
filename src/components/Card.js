7f1f8a5e95c91271b339b0736f9496ecb8f9f789
mohmedvaid <hussainvaid7@gmail.com>
2022-01-11 20:53:08 -0600

    added copy to clipboard link

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { BsHeartFill, BsHeart } from 'react-icons/bs';
+import React, { useState, useEffect } from 'react'
+import { BsHeartFill, BsHeart } from 'react-icons/bs'
 
 const Card = ({ photo, addNewLike, removeLike, isLikedImage }) => {
-  let data = photo.data[0];
-  let imgSrc = photo.links[0].href;
-  let dateCreated = new Date(data.date_created).toLocaleDateString('en-US');
-  let [isLiked, setIsLiked] = useState(isLikedImage);
-  const toggleLike = () => {
-    setIsLiked(!isLiked);
-  };
+	let data = photo.data[0]
+	let imgSrc = photo.links[0].href
+	let dateCreated = new Date(data.date_created).toLocaleDateString('en-US')
+	let description = data.description
+	let [isLiked, setIsLiked] = useState(isLikedImage)
+	const toggleLike = () => {
+		setIsLiked(!isLiked)
+	}
 
-  useEffect(() => {
-    if (isLiked) addNewLike(data.nasa_id);
-    else removeLike(data.nasa_id);
-  }, [isLiked]);
+	const copyImageUrl = async () => {
+		if (navigator.clipboard) {
+			await navigator.clipboard.writeText(imgSrc)
+		} else {
+			document.execCommand('copy', true, imgSrc)
+		}
+		alert('Image URL copied to clipboard')
+	}
 
-  return (
-    <div className="m-1">
-      <div className="card">
-        <img className="card-img-top img-fluid" src={imgSrc} alt={data.description} />
-        <div className="card-body">
-          <h4 className="card-title text-dark">{data.title}</h4>
-          <div className="d-flex justify-content-between my-1">
-            {/* disbaling text selection on ancher below as clicking like button multiple times was highligting the date text */}
-            <a className="likeBtn disable-text-selection" onClick={() => toggleLike()}>
-              {/* like icon */}
-              <span> {isLiked ? <BsHeartFill size="2em" color="red" /> : <BsHeart size="2em" color="red" />}</span>
-            </a>
-            <p className="card-text text-dark">
-              <small className="text-muted">{dateCreated}</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	useEffect(() => {
+		if (isLiked) addNewLike(data.nasa_id)
+		else removeLike(data.nasa_id)
+	}, [isLiked])
 
-export default Card;
+	return (
+		<div className="m-1">
+			<div className="card">
+				<img className="card-img-top img-fluid" src={imgSrc} alt={description} />
+				<div className="card-body">
+					<h4 className="card-title text-dark">{data.title}</h4>
+					<div className="d-flex justify-content-between my-1">
+						{/* disbaling text selection on ancher below as clicking like button multiple times was highligting the date text */}
+						<a className="likeBtn m-1 disable-text-selection" onClick={() => toggleLike()}>
+							{/* like icon */}
+							<span>
+								{' '}
+								{isLiked ? (
+									<BsHeartFill size="2em" color="red" />
+								) : (
+									<BsHeart size="2em" color="red" />
+								)}
+							</span>
+						</a>
+						<a className="copyBtn m-1 cursor-pointer" onClick={() => copyImageUrl()}>
+							Copy Image Link
+						</a>
+						<p className="card-text text-dark">
+							<small className="text-muted">{dateCreated}</small>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Card
